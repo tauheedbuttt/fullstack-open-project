849f12102e32b9d5dec0559054427e45b71df17d
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-15 19:38:22 +0300

    chore: clean up tsconfig files and remove unused references

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { IUser } from "@libs/shared";
 
 @Controller()
 export class AppController {
@@ -8,12 +7,6 @@ export class AppController {
 
   @Get()
   getData() {
-    const user: IUser = {
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@example.com",
-    };
-    console.log("User:", user);
     return this.appService.getData();
   }
 }
