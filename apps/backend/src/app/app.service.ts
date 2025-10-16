a2e09a774e58a7f238ee350ba3911ef507acaaae
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-16 17:19:00 +0300

    shared-libs

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { IUser } from "@libs/shared";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return ({ message: 'Hello API' });
+  getData(): { message: string; user: IUser } {
+    const user: IUser = {
+      name: "John Doe",
+      id: 30,
+      email: "john.doe@example.com",
+    };
+    return { message: "Hello API", user };
   }
 }
