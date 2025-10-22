312c5c60215923ebb6bcfa726c7e09bc8146af30
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-22 13:14:01 +0300

    refactor: Update import statements and restructure App component

---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
