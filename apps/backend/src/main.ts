fbecd01c87f8f372ad47b9dfd484d544c6ef615a
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-31 07:37:56 +0200

    feat(user): implement user module with controller and service feat(app): create app module and controller with basic data retrieval feat(app): add unit tests for app controller and service

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app/app.module";
+import { AppModule } from "./app.module";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
